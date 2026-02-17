a37f49b6dc443bd6b8d772e2f651208211caafc4
--replace-all <nobud@yandex.ru>
2026-02-17 19:51:11 +0300

    Создание БД и таблицы scheduler

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"go_final_project/pkg/db"
 	"log"
 	"net/http"
 )
@@ -10,15 +11,20 @@ func main() {
 	const port = 7540
 	const webDir = "./web"
 
+	dbFile := "scheduler.db"
+	err := db.Init(dbFile)
+	if err != nil {
+		log.Fatal("ошибка инициализации БД:", err)
+	}
+
 	fs := http.FileServer(http.Dir(webDir))
 
 	http.Handle("/", fs)
 
 	log.Printf("Сервер запущен на http://localhost:%d", port)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		log.Fatal("Ошибка запуска сервера:", err)
 	}
-
 }
---
+++ pkg/db/db.go
@@ -0,0 +1,45 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "modernc.org/sqlite"
+)
+
+var db *sql.DB
+
+const schema = `
+	CREATE TABLE IF NOT EXISTS scheduler (
+	    id INTEGER PRIMARY KEY AUTOINCREMENT,
+	    date CHAR(8) NOT NULL DEFAULT "",
+	    title VARCHAR(255) NOT NULL DEFAULT "",
+	    comment TEXT,
+	    repeat VARCHAR(128) NOT NULL DEFAULT ""
+	);
+	CREATE INDEX idx_scheduler_date ON scheduler(date);
+`
+
+func Init(dbFile string) error {
+	var install bool
+	_, err := os.Stat(dbFile)
+
+	if err != nil {
+		install = true
+	}
+
+	db, err = sql.Open("sqlite", dbFile)
+	if err != nil {
+		return fmt.Errorf("ошибка открытия БД %w", err)
+	}
+
+	if install {
+		_, err := db.Exec(schema)
+		if err != nil {
+			db.Close()
+			return fmt.Errorf("ошибка создания схемы БД %w", err)
+		}
+	}
+	return nil
+}
